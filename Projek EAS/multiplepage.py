733ffc67f11de24eedbf5c5aea889deed2efefb1
Divamia30 <130281183+Divamia30@users.noreply.github.com>
2023-06-13 02:45:24 +0700

    Add files via upload

---
+++ Projek EAS/multiplepage.py
@@ -0,0 +1,10 @@
+"""Menghitung Nilai Z Score"""
+
+import streamlit as st
+
+def main():
+    st.title("Menghitung Nilai Statistik Uji Proporsi Satu Populasi")
+    st.subheader("Z score adalah suatu ukuran penyimpangan data dari nilai rata-rata yang telah diukur dengan satuan standar deviasinya. Jika nilai yang diperoleh diatas rata-rata maka Z score-nya akan bernilai positif, sedangkan jika nilai yang diperoleh dibawah rata-rata maka Z score-nya bernilai negatif. Z score juga memiliki sebutan lain yaitu nilai standar atau nilai baku. ")
+
+if __name__ == '__main__':
+    main()
